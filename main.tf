c4319d42bdcb7b4468001b2721a37ba1f3215ee9
John Noss <john.noss@asics.com>
2019-09-27 17:06:08 -0400

    fix: invert count

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "aws_s3_bucket" "bucket" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_transfer_user" "transfer_user" {
-  count          = "${var.transfer_server_enable_password_auth ? 1 : 0}"
+  count          = "${var.transfer_server_enable_password_auth ? 0 : 1}"
   server_id      = "${var.transfer_server_id}"
   role           = "${aws_iam_role.transfer_user_assume_role.arn}"
   home_directory = "/${data.aws_s3_bucket.bucket.id}/${var.s3_bucket_folder}"
@@ -19,7 +19,7 @@ resource "aws_transfer_user" "transfer_user" {
 }
 
 resource "aws_transfer_ssh_key" "transfer_ssh_key" {
-  count     = "${var.transfer_server_enable_password_auth ? var.ssh_public_keys_length : 0}"
+  count     = "${var.transfer_server_enable_password_auth ? 0 : var.ssh_public_keys_length}"
   server_id = "${var.transfer_server_id}"
   user_name = "${aws_transfer_user.transfer_user.user_name}"
   body      = "${var.ssh_public_keys[count.index]}"
